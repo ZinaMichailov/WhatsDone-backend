18fe61342a46ec11655abd438cb50ec30cf3461a
zina <zinam84@gmail.com>
2021-03-29 11:43:09 +0300

    heroku

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  'dbURL': 'mongodb+srv://zina:WhatsDone@cluster0.7rdpb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 }
